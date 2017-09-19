a8388a88d5adff55a028e381ac897eaf55be1fcd
Michelle M Y ZOU <michelle.m.y.zou@hsbc.com>
2017-09-19 19:36:47 +0800

    Initial Commit

---
+++ michelle_viper_demo01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  michelle_viper_demo01
+//
+//  Created by Michelle on 19/09/2017.
+//  Copyright © 2017 Michelle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ michelle_viper_demo01Tests/michelle_viper_demo01Tests.swift
@@ -0,0 +1,36 @@
+//
+//  michelle_viper_demo01Tests.swift
+//  michelle_viper_demo01Tests
+//
+//  Created by Michelle on 19/09/2017.
+//  Copyright © 2017 Michelle. All rights reserved.
+//
+
+import XCTest
+@testable import michelle_viper_demo01
+
+class michelle_viper_demo01Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
